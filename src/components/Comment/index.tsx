a53ffe970d8e62083558eb64610c97b8a22ea2fe
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2025-08-07 00:55:46 -0300

    fix(Comment): estruturacao do html do comentario

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -8,3 +8,28 @@ interface CommentProps {
     texto: string;
     onDelete: () => void;
 }
+
+export default function Comment({nome, foto, tempo, texto, onDelete}: CommentProps) {
+    const[likes, setLikes] = useState(0);
+
+    function handleLide(){
+        setLikes(Like + 1);
+    }
+
+    return (
+        <div className={styles.comment}>
+            <img src={foto} alt="" className={styles.avatar}/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentHeader}>
+                    <div className={styles.nomeEtempo}>
+                        <div className={styles.nome}></div>
+                        <div className={styles.tempo}></div>
+                    </div>                    
+                </div>
+
+                
+            </div>
+        </div>
+    )
+}
